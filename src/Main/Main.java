38bb6015c049bb55f98bba2e1291643c699ca936
OleaBlossom <o.corcoran@gmail.com>
2022-01-26 20:34:20 +0100

    implement selection sort and insertion sort

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -7,9 +7,17 @@ public class Main {
     public static void main(String[] args) {
         SortAlgorithms s = new SortAlgorithms();
 
-        int[] arrayFromSelection = s.selectionSort();
+        int[] arrayFromSelection = s.selectionSort(15);
 
+        System.out.println("selection sort:");
         for (int j : arrayFromSelection) {
+            System.out.println(j);
+        }
+
+        int[] arrayFromInsertion = s.insertionSort(25);
+
+        System.out.println("insertion sort:");
+        for (int j : arrayFromInsertion) {
             System.out.println(j);
         }
     }
--- src/SortAlgorithms/SortAlgorithms.java
+++ src/SortAlgorithms/SortAlgorithms.java
@@ -1,35 +1,66 @@
 package SortAlgorithms;
 
 import java.util.Random;
+
 public class SortAlgorithms {
 
 
     /**
      * leave to last -- we don't have to do this one
+     *
      * @return int[]
      */
-    public int[] bubbleSort()
-    {
-        int[] myArray = new int[5];
-
+    public int[] bubbleSort(int size) {
+        int[] myArray = loadArrayOfRandomInts(size);
         return myArray;
     }
 
-    public int[] selectionSort()
-    {
-        int[] myArray = new int[5];
+    public int[] insertionSort(int size) {
+        int[] array = loadArrayOfRandomInts(size);
+
+        for (int i = 1; i < array.length; i++) {
+            int j = i - 1;
+            int current = array[i];
+
+            while (j >= 0 && current < array[j]) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+
+            array[j + 1] = current;
+        }
+
+        return array;
+    }
+
+    public int[] selectionSort(int size) {
+        int[] array = loadArrayOfRandomInts(size);
+
+        for (int i = 0; i < (array.length - 1); i++) {
+            int minPosition = i;
+
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minPosition]) {
+                    minPosition = j;
+                }
+            }
+
+            int tmp = array[minPosition];
+            array[minPosition] = array[i];
+            array[i] = tmp;
+        }
+
+        return array;
+    }
+
+    private int[] loadArrayOfRandomInts(int size) {
         Random rand = new Random();
-        int x = rand.nextInt();
+        int[] myArray = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            myArray[i] = rand.nextInt(1000);
+        }
 
         return myArray;
     }
-
-    public int[] insertionSort()
-    {
-        int[] myArray = new int[5];
-
-        return myArray;
-    }
-
-
 }
